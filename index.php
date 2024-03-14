32d211010ce2850683e391dfccde174830735a9e
GoldSenseiBoi <98558347+GoldSenseiBoi@users.noreply.github.com>
2024-03-14 23:36:50 +0100

    add :accept:

--- index.php
+++ index.php
@@ -10,32 +10,52 @@
 
 </head>
 <body>
-    <h1>Liste des destinations</h1>
-    <div class="destination-container">
-        
-        <?php
-        // Inclure le fichier de configuration de la base de données
-        require_once 'config/database.php';
+    <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php">
+                    <img src="images/AF.jpg"  alt="Logo" width="55" height="40" class="d-inline-block align-text-top">
+                    <h4>Air France</h4>
+                    </a>
+                </div>
+                </nav>
+    <div class="container">
+                
+                <br />
+                <br />
+                <div class="titre2">
+                <h2 style="">Bienvenue sur le site de Air France.</h2>
+                </div>
+                <br />
+                <br />
+                <br />
+                <div class="titre1">
+                    <h1>Nos destinations</h1>
+                </div>
+                <div class="destination-container">
+                    
+                    <?php
+                    // Inclure le fichier de configuration de la base de données
+                    require_once 'config/database.php';
 
-        // Récupérer la liste des destinations depuis la base de données
-        $query = "SELECT * FROM Destination";
-        $stmt = $db->query($query);
-        $destinations = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    // Récupérer la liste des destinations depuis la base de données
+                    $query = "SELECT * FROM Destination";
+                    $stmt = $db->query($query);
+                    $destinations = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        // Afficher chaque destination sous forme de card
-        foreach ($destinations as $destination) {
-            echo '<div class="card" style="width: 18rem; margin: 20px;">';
-            echo '<img style="width: 287px;height: 190px;" src="' . $destination['Image'] . '" class="card-img-top" alt="' . $destination['Nom_ville'] . '">';
-            echo '<div class="card-body">';
-            echo '<h5 class="card-title">' . $destination['Nom_ville'] . '</h5>';
-            echo '<a href="vols.php?destination_id=' . $destination['ID_Destination'] . '" class="btn btn-primary">Voir les vols</a>';
-            echo '</div>'; // Fermeture de card-body
-            echo '</div>'; // Fermeture de card
-        }
-        ?>
-        
+                    // Afficher chaque destination sous forme de card
+                    foreach ($destinations as $destination) {
+                        echo '<div class="card" style="width: 18rem; margin: 20px;">';
+                        echo '<img style="width: 287px;height: 190px;" src="' . $destination['Image'] . '" class="card-img-top" alt="' . $destination['Nom_ville'] . '">';
+                        echo '<div class="card-body">';
+                        echo '<h5 class="card-title">' . $destination['Nom_ville'] . '</h5>';
+                        echo '<a href="vols.php?destination_id=' . $destination['ID_Destination'] . '" class="btn btn-primary">Voir les vols</a>';
+                        echo '</div>'; // Fermeture de card-body
+                        echo '</div>'; // Fermeture de card
+                    }
+                    ?>
+                    
+                </div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
