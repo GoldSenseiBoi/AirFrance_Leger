09c3a4a72cb844a1f9d72806b3e567b88593eb19
GoldSenseiBoi <98558347+GoldSenseiBoi@users.noreply.github.com>
2024-04-05 01:38:43 +0200

    add :cat: :mouse:  tom and jerry

--- vue/vue_insert_menbreequipage.php
+++ vue/vue_insert_menbreequipage.php
@@ -3,38 +3,38 @@
     <table>
         <tr>
             <td>ID Membre d'équipage</td>
-            <td><input type="text" name="ID_MembreEquipage" value="<?= ($lMembre != null) ? $lMembre['ID_MembreEquipage'] : '' ?>"></td>
+            <td><input type="text" name="ID_MembreEquipage" value="<?= ($lMenbre != null) ? $lMenbre['ID_MembreEquipage'] : '' ?>"></td>
         </tr>
         <tr>
             <td>ID Personne</td>
-            <td><input type="text" name="ID_Personne" value="<?= ($lMembre != null) ? $lMembre['ID_Personne'] : '' ?>"></td>
+            <td><input type="text" name="ID_Personne" value="<?= ($lMenbre != null) ? $lMenbre['ID_Personne'] : '' ?>"></td>
         </tr>
         <tr>
             <td>Rôle</td>
             <td>
                 <select name="Role">
-                    <option value="Pilote" <?= ($lMembre != null && $lMembre['Role'] == 'Pilote') ? 'selected' : '' ?>>Pilote</option>
-                    <option value="Copilote" <?= ($lMembre != null && $lMembre['Role'] == 'Copilote') ? 'selected' : '' ?>>Copilote</option>
-                    <option value="Hôtesse de l'air" <?= ($lMembre != null && $lMembre['Role'] == 'Hôtesse de l\'air') ? 'selected' : '' ?>>Hôtesse de l'air</option>
-                    <option value="Steward" <?= ($lMembre != null && $lMembre['Role'] == 'Steward') ? 'selected' : '' ?>>Steward</option>
+                    <option value="Pilote" <?= ($lMenbre != null && $lMenbre['Role'] == 'Pilote') ? 'selected' : '' ?>>Pilote</option>
+                    <option value="Copilote" <?= ($lMenbre != null && $lMenbre['Role'] == 'Copilote') ? 'selected' : '' ?>>Copilote</option>
+                    <option value="Hôtesse de l'air" <?= ($lMenbre != null && $lMenbre['Role'] == 'Hôtesse de l\'air') ? 'selected' : '' ?>>Hôtesse de l'air</option>
+                    <option value="Steward" <?= ($lMenbre != null && $lMenbre['Role'] == 'Steward') ? 'selected' : '' ?>>Steward</option>
                 </select>
             </td>
         </tr>
         <tr>
             <td>Date d'embauche</td>
-            <td><input type="date" name="DateEmbauche" value="<?= ($lMembre != null) ? $lMembre['DateEmbauche'] : '' ?>"></td>
+            <td><input type="date" name="DateEmbauche" value="<?= ($lMenbre != null) ? $lMenbre['DateEmbauche'] : '' ?>"></td>
         </tr>
         <tr>
             <td>ID Vol</td>
-            <td><input type="text" name="ID_Vol" value="<?= ($lMembre != null) ? $lMembre['ID_Vol'] : '' ?>"></td>
+            <td><input type="text" name="ID_Vol" value="<?= ($lMenbre != null) ? $lMenbre['ID_Vol'] : '' ?>"></td>
         </tr>
         <tr>
             <td></td>
             <td>
-                <input type="submit" <?= ($lMembre != null) ? 'name="Modifier" value="Modifier"' : 'name="Valider" value="Valider"' ?>>
+                <input type="submit" <?= ($lMenbre != null) ? 'name="Modifier" value="Modifier"' : 'name="Valider" value="Valider"' ?>>
                 <input type="reset" name="Annuler" value="Annuler">
             </td>
         </tr>
-        <?= ($lMembre != null) ? '<input type="hidden" name="ID_MembreEquipage" value="'.$lMembre['ID_MembreEquipage'].'">' : '' ?>
+        <?= ($lMenbre != null) ? '<input type="hidden" name="ID_MembreEquipage" value="'.$lMenbre['ID_MembreEquipage'].'">' : '' ?>
     </table>
 </form>
